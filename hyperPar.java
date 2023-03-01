4885d81de62fa02e48bf0293b53bae5c0a763b7b
Sara <sjimenez11@educa.madrid.org>
2023-03-01 09:40:49 +0100

    Versión rama 1 - Creación conflicto

--- hyperPar.java
+++ hyperPar.java
@@ -1,11 +1,14 @@
 public static boolean hyperPar(int num){
     int resto;
-    while(num != 0){
-        resto = num%10;
-        num = num/10;
-        if(resto%2 != 0){
-            return false;
+    if(num >= 0){
+        while(num != 0){
+            resto = num%10;
+            num = num/10;
+            if(resto%2 != 0){
+                return false;
+            }
+            return true;
         }
-        return true;
     }
+    return false;
 }
